36315cfa8e088382f8b426797d13cd4fabe0bfbd
Jakob Essig <jecolousa@gmail.com>
2017-11-16 16:38:29 -0700

    Week 4 Changes

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -5,9 +5,28 @@ var ratingApp = angular.module('ratingApp', []);
 //controller 
 
 ratingApp.controller('viewRatings', ['$scope', function($scope){
-       $scope.rating={
+        $scope.filterHistory=[];
+        $scope.resetFilter = function(){
+            $scope.filterHistory.push($scope.filterText);
+    $scope.filterText = "Test Filter Delete This...";
+        };
+        
+        $scope.resetFilter();
+        $scope.filterHistory=[];
+        
+        $scope.ratings=[{
         userName: 'Steve',
         rating: 5,
         comments:'Excellent!'
-    };
+    },
+    {
+        userName: 'Anne',
+        rating: 3,
+        comments:'It was very meh'
+    },
+    {
+        userName: 'Lisa',
+        rating: 2,
+        comments:'It was pretty bad'
+    }];
 }]);
